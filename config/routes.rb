4a026130f5f7953eb1a22f7e17c1519bb159d10e
Vanessa <nassangavanessa2@gmail.com>
2025-04-10 12:43:07 +0300

    generated a controller, models

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Root route - maps http://localhost:3000 to the index action of ArticlesController
+  # defines the page that appears when you visit http://localhost:3000
+  root "articles#index"
+  
+  # Route for GET /articles - maps to the index action of ArticlesController
+  # defines the route for http://localhost:3000/articles
+  get "/articles", to: "articles#index"
 end
