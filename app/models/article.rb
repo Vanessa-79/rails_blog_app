15631056319a2c7f641468f58b836e79244349d7
Vanessa <nassangavanessa2@gmail.com>
2025-04-14 12:16:14 +0300

    added the crud

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
+
+# The first validation declares that a title value must be present. 
+# The second validation declares that a body value must also be present, it declares that the body value must be at least 10 characters long.
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   # defines the page that appears when you visit http://localhost:3000
   root "articles#index"
   
-  # Route for GET /articles - maps to the index action of ArticlesController
-  # defines the route for http://localhost:3000/articles
-  get "/articles", to: "articles#index"
+  resources :articles
+  # This line creates RESTful routes for the ArticlesController, which includes:
+  # - index: GET /articles
+  # - show: GET /articles/:id
+  # - new: GET /articles/new
+  # - create: POST /articles
+  # - edit: GET /articles/:id/edit
+  # - update: PATCH/PUT /articles/:id
+  # - destroy: DELETE /articles/:id
+  # The resources method automatically generates all the necessary routes for the controller actions.
+  # The resources method also sets up URL and path helper methods that we can use to keep our code from depending on a specific route configuration.
+
 end
